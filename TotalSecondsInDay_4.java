8c63088248b99aafecda76cfe96d0a1fe37202d6
muppidishivani <133558846+muppidishivani@users.noreply.github.com>
2024-04-11 17:14:45 -0700

    Add files via upload

---
+++ TotalSecondsInDay_4.java
@@ -0,0 +1,15 @@
+package com.web.java;
+
+public class TotalSecondsInDay_4 {
+
+	
+	    public static void main(String[] args) {
+		        int hoursInDay = 24;
+		        int minutesInHour = 60;
+		        int secondsInMinute = 60;
+
+		        int totalSecondsInDay = hoursInDay * minutesInHour * secondsInMinute;
+		        System. out. println("total seconds in a day="+totalSecondsInDay);
+		    }
+	}
+
